06cab46afed1c6cdea6589af58192c3712803004
Himani Nimma <it1298himani@gmail.com>
2026-05-29 12:43:37 +0530

    Time: 19 ms (19.13%) | Memory: 19.4 MB (67.47%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,12 +1,12 @@
 class Solution:
     def nextGreaterElement(self, n1: List[int], n2: List[int]) -> List[int]:
         stack=[]
-        for num in n1:
-            idx=n2.index(num)
-            nxt=-1
+        for curr in n1:
+            idx=n2.index(curr)
+            greater=-1
             for j in range(idx+1,len(n2)):
-                if n2[j]>num:
-                    nxt=n2[j]
+                if n2[j]>curr:
+                    greater=n2[j]
                     break
-            stack.append(nxt)
+            stack.append(greater)
         return stack
